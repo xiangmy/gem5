934e159769137e0891aa3c52a762fa2c1e6ab284
Mingyuan <xiang_my@outlook.com>
2019-07-15 00:58:37 -0500

    fix(mem-ruby): segment fault caused by instantiating replacementData in Ruby
    Mistakenly, instantiate replacementData in CacheMemory::init(), which will cause
    a segment fault. Calling instantiateEntry() in CacheMemory::allocate() will
    solve this problem.
    
    Change-Id: Ic9600519cb22fb3463e9823a3bb3cfa8e745e5e1

--- src/mem/ruby/structures/CacheMemory.cc
+++ src/mem/ruby/structures/CacheMemory.cc
@@ -85,12 +85,6 @@ CacheMemory::init()
 
     m_cache.resize(m_cache_num_sets,
                     std::vector<AbstractCacheEntry*>(m_cache_assoc, nullptr));
-    for (int set = 0; set < m_cache_num_sets; ++set){
-        for (int assoc = 0; assoc < m_cache_assoc; ++assoc){
-            m_cache[set][assoc]->replacementData = m_replacementPolicy_ptr->
-                                                         instantiateEntry();
-        }
-    }
 }
 
 CacheMemory::~CacheMemory()
@@ -286,6 +280,8 @@ CacheMemory::allocate(Addr address, AbstractCacheEntry *entry, bool touch)
             m_tag_index[address] = i;
             entry->setSetIndex(cacheSet);
             entry->setWayIndex(i);
+            set[i]->replacementData =
+                               m_replacementPolicy_ptr->instantiateEntry();
 
             if (touch) {
                 m_replacementPolicy_ptr->
